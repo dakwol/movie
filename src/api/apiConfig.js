93f2433aa6de1a374c13cd9f28fadb9f4f5278d0
Dakwol <daniilfomenko99@gmail.com>
2022-01-11 13:16:36 +0300

    api

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: 'htttps://api.themoviedb.org/3/',
+    apiKey: '1cc28d7cb8202fa7566afa90c4a8b9f4',
+    orignalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig;
