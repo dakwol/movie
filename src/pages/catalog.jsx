ef437f7e1b4943f92be5d4d70c6e2a8ff7f3de24
Dakwol <daniilfomenko99@gmail.com>
2022-01-15 18:19:10 +0300

    master

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -5,17 +5,23 @@ import { useParams } from "react-router-dom";
 import PageHeader from "../components/page-header/PageHeader";
 
 import { category as cate } from '../api/tmdbApi';
+import MovieGrid from "../components/movie-grid/MovieGrid";
 
 const Catalog = () => {
 
     const { category } = useParams();
 
     return (
-        <div>
+        <>
             <PageHeader>
                 {category === cate.movie ? 'Movies' : 'TV Series'}
             </PageHeader>
-        </div>
+            <div className="container">
+                <div className="section mb-3">
+                    <MovieGrid category={category}/>
+                </div>
+            </div>
+        </>
     );
 }
 
